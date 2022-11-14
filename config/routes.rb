803842cb611a0e37d66e62396c2a4288f87da9c1
Brian Palladino <bdpalladino@gmail.com>
2022-11-14 12:45:26 -0500

    Completed step 7 CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "articles#index"
   
-  get "/articles", to: "articles#index"
+  resources :articles
 end
+
+# Reference:
+# https://guides.rubyonrails.org/routing.html
+# https://api.rubyonrails.org/v7.0.4/classes/ActionDispatch/Routing/Mapper/Resources.html#method-i-resources
