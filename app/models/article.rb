ae945f5b6673e565b6131279b3c6c247e83209ba
Brian Palladino <bdpalladino@gmail.com>
2022-11-18 12:27:52 -0500

    Add article status validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,7 @@ class Article < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
+  validates :status, presence: true
 end
 
 # Reference:
